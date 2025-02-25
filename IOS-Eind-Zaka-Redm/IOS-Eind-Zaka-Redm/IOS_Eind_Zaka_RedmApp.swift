1ff41ba2ef9578cccdc9259652fdb649903e6ce5
Redmar Alkema <rag.alkema@student.avans.nl>
2025-02-25 10:08:09 +0100

    test test

---
+++ IOS-Eind-Zaka-Redm/IOS-Eind-Zaka-Redm/IOS_Eind_Zaka_RedmApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Eind_Zaka_RedmApp.swift
+//  IOS-Eind-Zaka-Redm
+//
+//  Created by Redmar Alkema on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Eind_Zaka_RedmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-Eind-Zaka-Redm/IOS-Eind-Zaka-RedmTests/IOS_Eind_Zaka_RedmTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Eind_Zaka_RedmTests.swift
+//  IOS-Eind-Zaka-RedmTests
+//
+//  Created by Redmar Alkema on 19/02/2025.
+//
+
+import Testing
+@testable import IOS_Eind_Zaka_Redm
+
+struct IOS_Eind_Zaka_RedmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
