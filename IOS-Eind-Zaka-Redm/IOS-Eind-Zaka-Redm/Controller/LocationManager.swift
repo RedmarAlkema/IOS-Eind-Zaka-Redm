fac4970abc7b3043af30f7c0160642d1eabb5c8d
Redmar Alkema <rag.alkema@student.avans.nl>
2025-02-26 14:30:56 +0100

    toevoegen van gps, misschien te veel werk dus in de toekomst laten zitten

---
+++ IOS-Eind-Zaka-Redm/IOS-Eind-Zaka-Redm/Controller/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  IOS-Eind-Zaka-Redm
+//
+//  Created by Redmar Alkema on 26/02/2025.
+//
+
+import Foundation
---
+++ IOS-Eind-Zaka-Redm/IOS-Eind-Zaka-Redm/Model/Extensions/CLLocationCoordinate2D+Codable.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  IOS-Eind-Zaka-Redm
+//
+//  Created by Redmar Alkema on 26/02/2025.
+//
+
